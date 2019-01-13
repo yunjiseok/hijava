71f904bfa04c484a31854da8440bf64e546ae22a
60308 <60308@60308-PC>
2019-01-13 13:34:31 +0900

    l

--- src/hijava/practiec/Nine.java
+++ src/hijava/practiec/Nine.java
@@ -4,7 +4,7 @@ public class Nine {
 
 	public static void main(String[] args) {
 		for(int j=2; j<20; j++) {
-			for(int i=1; i<20; i++) {
+			for(int i=1; i<20 ; i++) {
 				System.out.println(j+"x"+i+"="+j*i );
 			}
 			System.out.println("\n");//단이 끝날 때마다 뜀
