47dca9c778cd0f971713fc6b8bd52eb720011df7
60308 <60308@60308-PC>
2019-01-13 13:48:16 +0900

    t

---
+++ src/hijava/practiec/Nk.java
@@ -0,0 +1,19 @@
+package hijava.practiec;
+
+public class Nk {
+
+	public static void main(String[] args) {
+//		int x = 10;
+//		float y = 2.0f;
+//		
+//		
+//		System.out.println((int)(x+y));
+//		System.out.println((int)(x-y));
+//		System.out.println((int)(x*y));
+//		System.out.println((int)(x/y));
+		
+		System.out.println((int)(12.3 % 3));
+
+	}
+
+}
