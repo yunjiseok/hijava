46344f23a7979c6177775d6b431bf27bd55fab99
yunjiseok <60308@60308-PC>
2019-01-19 10:42:01 +0900

    f

---
+++ src/hijava/practiec/L.java
@@ -0,0 +1,19 @@
+package hijava.practiec;
+
+public class L {
+
+	public static void main(String[] args) {
+		int num =10;
+		int num2 = 20;
+		boolean result;
+		
+		result = ((num > 10) && (num2 > 20));
+		System.out.println(result);
+		result = ((num > 10) || (num2 > 20));
+		System.out.println(result);
+		System.out.println(!result);
+		
+		
+	}
+
+}
---
+++ src/hijava/practiec/Switch1.java
@@ -0,0 +1,25 @@
+package hijava.practiec;
+
+public class Switch1 {
+
+	public static void main(String[] args) {
+		long ctm = System.currentTimeMillis() ;
+		System.out.println("ctm=" + ctm);
+		
+		long sct = ctm % 3;
+		
+		switch( (int)sct ) {
+			case 1:
+				System.out.println("One");
+				break;
+			case 2:
+				System.out.println("Two");
+				break;
+			default:
+				System.out.println("Zero");
+		}
+
+
+	}
+
+}
