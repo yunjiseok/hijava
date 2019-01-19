627fa30f03b5338aa16495a0507e17cc75a26352
60308 <60308@60308-PC>
2019-01-19 09:49:02 +0900

    +

--- src/hijava/practiec/Nine.java
+++ src/hijava/practiec/Nine.java
@@ -3,8 +3,8 @@ package hijava.practiec;
 public class Nine {
 
 	public static void main(String[] args) {
-		for(int j=2; j<20; j++) {
-			for(int i=1; i<20 ; i++) {
+		for(int j=2; j<=9; j++) {
+			for(int i=1; i<=9 ; i++) {
 				System.out.println(j+"x"+i+"="+j*i );
 			}
 			System.out.println("\n");//단이 끝날 때마다 뜀
---
+++ src/hijava/practiec/Oper.java
@@ -0,0 +1,19 @@
+package hijava.practiec;
+
+public class Oper {
+
+	public static void main(String[] args) {
+		int i = 0;
+		i /= 2;
+		System.out.println(i);
+		
+		int k = !(i<0) ? 100 : 1000;
+		System.out.println("k=" + k);
+//		i++;
+//		i--;
+//		int j = i++;
+//		System.out.println(i++ + ", j=" + j);
+
+	}
+
+}
