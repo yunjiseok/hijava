e32355b35f35cd96b5a0143c6895dc207ae81e67
60308 <60308@60308-PC>
2019-02-09 13:47:50 +0900

    y

--- src/hijava/oop/CalcImpl.java
+++ src/hijava/oop/CalcImpl.java
@@ -12,4 +12,14 @@ public class CalcImpl implements Calc {
 		System.out.println(x - y);
 	}
 
+	@Override
+	public void mul(int x, int y) {
+		System.out.println(x * y);
+	}
+
+	@Override
+	public void div(int x, int y) {
+		System.out.println(x / y);
+	}
+
 }
