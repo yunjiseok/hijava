5b0306f70c02769badab03ba21bb58b816da71a7
60308 <60308@60308-PC>
2019-02-09 12:30:38 +0900

    s

--- src/hijava/oop/Calculator.java
+++ src/hijava/oop/Calculator.java
@@ -7,5 +7,6 @@ public interface Calculator {
 	void mul(int x, int y);
 	void div(int x, int y);
 	
+	
 
 }
--- src/hijava/oop/CalculatorImpl.java
+++ src/hijava/oop/CalculatorImpl.java
@@ -6,31 +6,28 @@ public class CalculatorImpl implements Calculator{
 
 	@Override
 	public void add(int x, int y) {
-		// TODO Auto-generated method stub
 		System.out.println(x + y);
 		
 	}
 
 	@Override
 	public void sub(int x, int y) {
-		// TODO Auto-generated method stub
 		System.out.println(x - y);
 		
 	}
 
 	@Override
 	public void mul(int x, int y) {
-		// TODO Auto-generated method stub
 		System.out.println(x * y);
 	}
 
 	@Override
 	public void div(int x, int y) {
-		// TODO Auto-generated method stub
 		if (y==0) {
 			System.out.println("no");
 		}else {
 		System.out.println(x / y);
+		
 	}
 	}
 }
